ba7f1d78f0791d605391df5b0ff99479f87bb61c
Leila Nazarli <116263067+leilanazarli@users.noreply.github.com>
2023-04-07 02:01:11 -0700

    Add files via upload

---
+++ 06.04/06.js
@@ -0,0 +1,26 @@
+let text = "there";
+
+console.log(`hello ${3+8}`);
+
+console.log(`hello ${'3+8'}`);
+
+console.log(`hello ${"text"}`);
+
+console.log(`hello ${text}`);
+
+
+//create variables named firstName , lastName age city uni
+let firstName = "Leyla"
+let lastName = "Nazarli"
+let age = 21
+let city = "Ganja"
+let uni = "GanjaStateUniversity"
+let info =  `${firstName} ${lastName} ${age} ${city} ${uni} `
+console.log(info)
+
+//
+let num1 = 5
+let num2 = "5"
+console.log ( num1 == num2)
+console.log ( num1 === num2)
+console.log ( num1 === + bnum2)
