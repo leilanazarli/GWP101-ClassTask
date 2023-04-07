88268bee631e49deaded88af5959cd96e238aea6
Leila Nazarli <116263067+leilanazarli@users.noreply.github.com>
2023-04-07 02:01:00 -0700

    Add files via upload

---
+++ 07.04/app.js
@@ -0,0 +1,18 @@
+// for (let i = 1; i <= 20; i++ ) {
+//     if ( i == 6 || i==7 ){
+//         continue;
+
+//     }
+//     console.log(i);
+// }
+// for( let i= 0 ; i <= 10 ;i++ ){
+//     console.log(`${i}x${i}=${i*i}`)
+// }
+// for( let i= 0 ; i <= 10 ;i++ ){
+//     console.log(`${i}x${i}=${i*i*i}`)
+// }
+// let index = 0 ;
+// while (index<=10){
+//     console.log(index);
+// }
+// console.log(index);
---
+++ 07.04/app2.js
@@ -0,0 +1,18 @@
+let myName = "leyla";
+for (let i = 0; i < myName.length; i++) {
+  console.log(myName[i]);
+}
+
+for (let a = 0; a < 8; a++) {
+  if (a % 2 == 0) {
+    console.log(a);
+  }
+}
+for(let b = 0 ; b < 7 ; b++){
+    if(b % 2 == 0){
+        console.log(`${b} even `);
+      }
+      else{
+           console.log(`${b} odd`) } 
+   
+}
