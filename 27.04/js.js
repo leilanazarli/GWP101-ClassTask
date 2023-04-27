43a29736de91c587afaefce66be99f18eb71040e
Leila Nazarli <116263067+leilanazarli@users.noreply.github.com>
2023-04-27 13:59:18 +0400

    Add files via upload

---
+++ 27.04/js.js
@@ -0,0 +1,59 @@
+const namee = document.querySelector("#name")
+const surname = document.querySelector("#surname")
+const email = document.querySelector("#email")
+const password = document.querySelector("#password")
+const checkBox = document.querySelector("#checkBox")
+const checkedLabel = document.querySelector(".checkedLabel")
+const radioinput = document.querySelectorAll(".radio")
+const submit =document.querySelector(".submit")
+namee.required = true
+surname.required = true
+email.required = true
+password.required = true
+
+namee.addEventListener("focus", function () {
+    this.style.background = "grey"
+    this.style.color = "blue"
+})
+namee.addEventListener("mouseout", function () {
+    this.style.background = "transparent"
+})
+surname.addEventListener("mouseover", function () {
+    this.style.background = "green"
+})
+surname.addEventListener("mouseout", function () {
+    this.style.background = "transparent"
+})
+email.addEventListener("contextmenu", function () {
+    this.style.background = "blue"
+})
+email.addEventListener("mouseover", function () {
+    this.style.background = "transparent"
+})
+
+password.addEventListener("focus", function () {
+    //   password.target.value 
+      this.value="****"
+})
+password.addEventListener("mouseover", function () {
+    this.value=""
+})
+checkBox.addEventListener('click', () => {
+    if (checkBox.checked) {
+        checkedLabel.style.visibility = 'visible'
+    }else{
+        checkedLabel.style.visibility = 'hidden'
+    }
+})
+
+radioinput.forEach(element => {
+    element.addEventListener("click",function(){
+        alert("Hello")
+    });
+})
+submit.id = "submit";
+document.getElementById("submit").disabled = true;
+checkBox.addEventListener("click" , function(){
+    document.getElementById("submit").disabled = false;
+
+})
