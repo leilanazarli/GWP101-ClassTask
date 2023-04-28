b27f2763c102158cfa53ba14901689397ab7a9c3
Leila Nazarli <116263067+leilanazarli@users.noreply.github.com>
2023-04-28 14:19:33 +0400

    Add files via upload

---
+++ 28.04/js.js
@@ -0,0 +1,9 @@
+// let input = document.querySelector("input")
+// let btn = document.querySelector(".add")
+// let ul = document.querySelector("ul")
+
+// btn.addEventListener("click",function(){
+//        let liElem=document.createElement("li")
+//        liElem.innerText="
+//        "
+// })
