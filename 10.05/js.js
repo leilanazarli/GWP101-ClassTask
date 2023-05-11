078e35b7bd818bd0ba7e6165459c4835148d016a
Leila Nazarli <116263067+leilanazarli@users.noreply.github.com>
2023-05-11 09:11:16 +0400

    Add files via upload

--- 10.05/js.js
+++ 10.05/js.js
@@ -12,9 +12,22 @@ let searchinput =document.querySelector("#searchinput")
 const thfirstname=document.querySelector('#thfirstname')
 let arrowup =document.querySelector(".arrowup")
 let arrowdown =document.querySelector(".arrowdown")
+let notification =document.querySelector("#notification")
 
+const alert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+    //   '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('')
+  
+    notification.append(wrapper)
+  }
+   
 
-axios("http://localhost:3000/users").then((res)=>{
+axios("http://localhost:6300/users").then((res)=>{
     // console.log(res.data);
     getData(res.data)
 
@@ -38,78 +51,101 @@ axios("http://localhost:3000/users").then((res)=>{
     });
 }
 async function deleteData(id){
-    await  fetch(`http://localhost:3000/users/${id}`, {
+    await  fetch(`http://localhost:6300/users/${id}`, {
         method: "DELETE"
       } )
+      
+        alert("Danger!!! ,U deleted your data" ,"danger")
+   
 }
 
+let status=false
+let editedId = null
+
 async function edit(id){
-    await  fetch(`http://localhost:3000/users/${id}`)
+     status=true
+     editedId=id
+    await  fetch(`http://localhost:6300/users/${editedId}`)
     .then((res)=>res.json()
     .then((data)=>{
         firstname.value=data.name;
         lastname.value=data.username;
         email.value=data.email;
+        submit.innerHTML="Edit"
 
-     form.addEventListener("submit" , function(event){
-          event.preventDefault()
-          let obj={
-            name:firstname.value,
-            username:lastname.value,
-            email:email.value
-          }
-         axios.patch(`http://localhost:3000/users/${id}`,obj)
-
-        })
     })
-   )}
-
-searchinput.addEventListener("input" ,function(event){
-    axios(`http://localhost:3000/users`)
-    .then((res)=>{  
-        console.log(res.data);
-       let filteredData= res.data.filter((item)=>{
-           return item.name.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()) || item.username.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()) 
-      }  )
-      getData(filteredData)
+    )}
+    form.addEventListener("submit" , function(event){
+         event.preventDefault()
+         let obj={
+           name:firstname.value,
+           username:lastname.value,
+           email:email.value
+         }
+            if(status){ 
+            //    console.log(status);
+               axios.patch(`http://localhost:6300/users/${editedId}`,obj)
+               alert("Good job! ,Your data has been edited" ,"warning")
+            }  
+            else{
+                axios.post("http://localhost:6300/users",obj)
+                alert("Good job! ,Your data has been added" ,"success")
+            }
+       })
+ let  searchSort=null
+ let arr=[]
+       searchinput.addEventListener("input" ,function(event){
+        searchSort=true   
+        axios(`http://localhost:6300/users`)
+           .then((res)=>{  
+               console.log(res.data);
+        arr= res.data.filter((item)=> item.name.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()) || item.username.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase())  )
+    // console.log(arr);
+      getData(arr)
     })
     })
 
    let bool =false
     thfirstname.addEventListener('click',()=>{
         bool=!bool
-         if(bool){
+        if(searchSort){
+                 if(bool){
+                    arrowdown.style.display='inline-block'
+                    arrowup.style.display='none'
+               sortedArr= arr.sort((a,b)=>a.name.localeCompare(b.name))
+               getData(sortedArr)
+            }
+                 else{
+                        arrowup.style.display='inline-block'
+                        arrowdown.style.display='none'
+                        sortedArr= arr.sort((a,b)=>b.name.localeCompare(a.name))
+                        getData(sortedArr)
+                    }
+        
+            }
+       else{
+        if(bool){
             arrowdown.style.display='inline-block'
             arrowup.style.display='none'
-            axios(`http://localhost:3000/users`)
+            axios(`http://localhost:6300/users`)
     .then((res)=>{  
        let sortedData= res.data.sort((a,b)=>a.name.localeCompare(b.name))
        getData(sortedData)
     })
          }
-         else{
+         else {
                 arrowup.style.display='inline-block'
                 arrowdown.style.display='none'
-                axios(`http://localhost:3000/users`)
+                axios(`http://localhost:6300/users`)
                 .then((res)=>{  
                    let sortedData= res.data.sort((a,b)=>b.name.localeCompare(a.name))
                    getData(sortedData)
                 })
             }
+       }
 
     })
 
-form.addEventListener("submit",function(){
-    try{
-        axios.post("http://localhost:8000/users",obj)
-    }
-    catch(err){
-        console.log(err);
-    }
-    // addUsers({
-    //     name:firstname.value,
-    //     username:lastname.value,
-    //     email:email.value
-    //    })
-})
+
+
     
